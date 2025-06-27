1e1c29853095dff00fdd1187c35d7bbafed31961
KTDVIP2k3 <Khanhtranduy996@gmail.com>
2025-06-27 20:49:36 +0700

    Example class interface show info for every class need to perform the detail of method showInfo()

---
+++ Pratice/Abstraction/Abstraction_Interface_Class/src/main/java/Interface/ShowInforInterface.java
@@ -0,0 +1,5 @@
+package Interface;
+
+public interface ShowInforInterface {
+    public void showInfo();
+}
---
+++ Pratice/Abstraction/Abstraction_Interface_Class/src/main/java/Models/Engine.java
@@ -0,0 +1,70 @@
+package Models;
+
+import Interface.ShowInforInterface;
+
+public class Engine implements ShowInforInterface {
+    private String model;
+    private int horsepower;
+    private double capacity; // dung tích (lít)
+    private String fuelType; // loại nhiên liệu: xăng, dầu, điện
+    private boolean isRunning;
+
+    // Constructor không tham số
+    public Engine() {
+    }
+
+    // Constructor đầy đủ tham số
+    public Engine(String model, int horsepower, double capacity, String fuelType) {
+        this.model = model;
+        this.horsepower = horsepower;
+        this.capacity = capacity;
+        this.fuelType = fuelType;
+        this.isRunning = false;
+    }
+
+    // Getters & Setters
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getHorsepower() {
+        return horsepower;
+    }
+
+    public void setHorsepower(int horsepower) {
+        this.horsepower = horsepower;
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(double capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Engine Model: " + model);
+        System.out.println("Horsepower: " + horsepower + " HP");
+        System.out.println("Capacity: " + capacity + " L");
+        System.out.println("Fuel Type: " + fuelType);
+        System.out.println("Status: " + (isRunning ? "Running" : "Stopped"));
+    }
+}
