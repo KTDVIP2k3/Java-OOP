09d579b243ee7f5e444c9836021c171132da2250
KTDVIP2k3 <Khanhtranduy996@gmail.com>
2025-06-27 17:56:06 +0700

    On goin do project book management in encapsulation. Project is divided into each folder to apply SOLID each class have their mission.

---
+++ Pratice/Encapsulation/Project_BookManagement/src/main/java/org/example/BookMagementService/BookManagement.java
@@ -0,0 +1,4 @@
+package org.example.BookMagementService;
+
+public class BookManagement {
+}
---
+++ Pratice/Encapsulation/Project_BookManagement/src/main/java/org/example/Models/Book.java
@@ -0,0 +1,75 @@
+package org.example.Models;
+
+public class Book {
+    private String tilte;
+    private String author;
+    private String description;
+    private int quantity;
+    private double price;
+    private boolean isAvailabe;
+
+    public Book(){}
+
+    public Book(String tilte, String author, String description, int quantity, double price, boolean isAvailabe) {
+        this.tilte = tilte;
+        this.author = author;
+        this.description = description;
+        this.quantity = quantity;
+        this.price = price;
+        this.isAvailabe = isAvailabe;
+    }
+
+    public String getTilte() {
+        return tilte;
+    }
+
+    public void setTilte(String tilte) {
+        this.tilte = tilte;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isAvailabe() {
+        return isAvailabe;
+    }
+
+    public void setAvailabe(boolean availabe) {
+        isAvailabe = availabe;
+    }
+
+    public void showInforBook(){
+      System.out.println("Title: " + this.tilte + " - " + "Author: "  + this.author +
+              " - " + "Description: " +  this.description + " - " + "Quantity: " + this.quantity
+              + " - " + "Price: " + this.price + " - " + "Avaialbe: " + this.isAvailabe);
+    }
+}
