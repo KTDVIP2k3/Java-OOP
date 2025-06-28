e3b4a5d631f227f0168147f922315f55a4411356
KTDVIP2k3 <Khanhtranduy996@gmail.com>
2025-06-28 08:39:51 +0700

    Create a subclass FullTimeEmployee extends abstract class Employee

---
+++ Pratice/Abstraction/Project_ManageEmoloyee/src/main/java/Models/FullTimeEmployee.java
@@ -0,0 +1,35 @@
+package Models;
+
+public class FullTimeEmployee extends Employee{
+    private double monthSalary;
+
+    public FullTimeEmployee() {
+    }
+
+    public FullTimeEmployee(String employeeId, String name, String address, String phone, double monthSalary) {
+        super(employeeId, name, address, phone);
+        this.monthSalary = monthSalary;
+    }
+
+    public double getMonthSalary() {
+        return monthSalary;
+    }
+
+    public void setMonthSalary(double monthSalary) {
+        this.monthSalary = monthSalary;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Employee Id: " + this.getEmployeeId());
+        System.out.println("Name: " + this.getName());
+        System.out.println("Address: " + this.getAddress());
+        System.out.println("Phone: " + this.getPhone());
+        System.out.println("Salary: " + calculateSalary());
+    }
+
+    @Override
+    public double calculateSalary() {
+        return this.monthSalary;
+    }
+}
