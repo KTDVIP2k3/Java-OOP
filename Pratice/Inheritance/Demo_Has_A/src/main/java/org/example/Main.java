ecc70abe1f940115019420939d0c5e70de1b9692
KTDVIP2k3 <Khanhtranduy996@gmail.com>
2025-06-28 17:02:46 +0700

    Complete demo inheritance has_a UI

--- Pratice/Inheritance/Demo_Has_A/src/main/java/org/example/Main.java
+++ Pratice/Inheritance/Demo_Has_A/src/main/java/org/example/Main.java
@@ -1,17 +1,79 @@
 package org.example;
 
+import Models.Laptop;
+import Models.Student;
+import Service.LaptopService;
+import Service.StudentService;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        StudentService studentService = new StudentService();
+        LaptopService laptopService = new LaptopService();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (true) {
+            System.out.println("===== Student-Laptop Management =====");
+            System.out.println("1. Add Student");
+            System.out.println("2. Add Laptop to Student");
+            System.out.println("3. Show All Students & Laptops");
+            System.out.println("4. Update Laptop Specs");
+            System.out.println("5. Exit");
+            System.out.print("Your choice: ");
+            int choice = Integer.parseInt(sc.nextLine());
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter student ID: ");
+                    String id = sc.nextLine();
+                    System.out.print("Enter student name: ");
+                    String name = sc.nextLine();
+                    studentService.addStudent(new Student(id, name));
+                    break;
+                case 2:
+                    System.out.print("Enter student ID: ");
+                    String sid = sc.nextLine();
+                    Student stu = studentService.findStudentById(sid);
+                    if (stu == null) {
+                        System.out.println("Student not found!");
+                        break;
+                    }
+                    System.out.print("Enter Laptop serial: ");
+                    String serial = sc.nextLine();
+                    System.out.print("Enter brand: ");
+                    String brand = sc.nextLine();
+                    System.out.print("Enter specs: ");
+                    String specs = sc.nextLine();
+                    laptopService.addLaptopToStudent(stu, new Laptop(serial, brand, specs));
+                    break;
+                case 3:
+                    studentService.showAllStudents();
+                    break;
+                case 4:
+                    System.out.print("Enter student ID: ");
+                    String stuId = sc.nextLine();
+                    Student s = studentService.findStudentById(stuId);
+                    if (s == null) {
+                        System.out.println("Student not found!");
+                        break;
+                    }
+                    System.out.print("Enter serial number to update: ");
+                    String updateSerial = sc.nextLine();
+                    System.out.print("Enter new specs: ");
+                    String newSpecs = sc.nextLine();
+                    if (laptopService.updateLaptopSpecs(s, updateSerial, newSpecs)) {
+                        System.out.println("Update success!");
+                    } else {
+                        System.out.println("Laptop not found!");
+                    }
+                    break;
+                case 5:
+                    System.out.println("Goodbye!");
+                    return;
+            }
         }
     }
 }
